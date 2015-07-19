faeaacd2f567b20630c6333373f259b00e289ed5
gnishida <gnishida@purdue.edu>
2015-07-19 18:23:55 -0400

    update glutils

--- GLUtils.h
+++ GLUtils.h
@@ -7,11 +7,9 @@
 
 namespace glutils {
 
-void drawQuad(const glm::vec3& c, float l, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
-void drawSphere(const glm::vec3& c, float r, const glm::vec3& color);
-void drawSphere(const glm::vec3& c, float r, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
-void drawCylinder(const glm::vec3& p, float h, float r, const glm::vec3& color);
-void drawCylinder(const glm::vec3& p, float h, float r, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
-void drawCone(const glm::vec3& p, float h, float r1, float r2, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
+void drawCircle(float r1, float r2, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
+void drawQuad(float l, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
+void drawSphere(float radius, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
+void drawCylinder(float radius1, float radius2, float h, const glm::vec3& color, const glm::mat4& mat, std::vector<Vertex>& vertices);
 
 }
